1f0f5d56046e9c7128ae98b9a4074a6461211700
outlife <outlifeatwork@gmail.com>
2025-09-09 20:52:27 +0530

    add form section

--- app/src/section/FormSection.jsx
+++ app/src/section/FormSection.jsx
@@ -3,9 +3,9 @@ import caretdown from '/caretdown.jpg';
 
 const FormSection = () => {
   return (
-    <div className='h-full w-[40%] px-8'>
+    <div className='h-full w-[45%] pl-6 pr-3 overflow-hidden'>
 
-      <div className='mt-[28px]'>
+      <div className='mt-[22px]'>
         <div className='font-poppins font-medium text-[26px] flex items-center gap-2'>
           Lets define your
           <div className='bg-gradient-to-r from-[#FFC835] to-[#FFA81C] bg-clip-text text-transparent'>
@@ -18,35 +18,55 @@ const FormSection = () => {
         </div>
       </div>
 
-      <div className='mt-[24px] flex flex-col'>
-        <div className='font-poppins font-normal text-[18px]'>Company Profile</div>
+      <div className='mt-[16px] flex flex-col'>
+        <div className='w-full flex items-start gap-4'>
+          <div className='w-full flex flex-col'>
+            <div className='font-poppins font-normal text-[18px]'>Company Profile</div>
+            <div className='mt-[16px] border-gray-200 border-2 p-4 flex flex-col gap-6 rounded-xl'>
 
-        <div className='mt-[16px] border-gray-200 border-2 p-6 flex flex-col gap-6 rounded-xl'>
-          <div className='flex flex-col gap-2'>
-            <div className='font-poppins font-normal text-[13px] leading-relaxed text-[#505050]'>Company Headcount</div>
-            <div className='w-full flex items-center justify-between rounded-lg px-4 py-2 border-gray-200 border-2'>
-              <div className='font-poppins font-normal text-[13px] leading-relaxed text-[#6b6b6b]'>1-10...</div>
-              <img src={caretdown} className='h-2 w-3' />
+              <div className='flex flex-col gap-3'>
+                <div className='font-poppins font-normal text-[13px] leading-relaxed'>Company Headcount</div>
+                <div className='w-full flex items-center justify-between rounded-lg px-4 py-2 border-gray-200 border-2'>
+                  <div className='font-poppins font-normal text-[13px] leading-relaxed text-[#8c8c8c]'>1-10...</div>
+                  <img src={caretdown} className='h-2 w-3' />
+                </div>
+              </div>
+              <div className='flex flex-col gap-3'>
+                <div className='font-poppins font-normal text-[13px] leading-relaxed '>Company HQ Locations</div>
+                <div className='w-full flex items-center justify-between rounded-lg px-4 py-2 border-gray-200 border-2'>
+                  <div className='font-poppins font-normal text-[13px] leading-relaxed text-[#8c8c8c]'>India...</div>
+                  <img src={caretdown} className='h-2 w-3' />
+                </div>
+              </div>
+              <div className='flex flex-col gap-3'>
+                <div className='font-poppins font-normal text-[13px] leading-relaxed'>Revenue range</div>
+                <div className='w-full flex items-center justify-between rounded-lg px-4 py-2 border-gray-200 border-2'>
+                  <div className='font-poppins font-normal text-[13px] leading-relaxed text-[#8c8c8c]'>50L - 1Cr...</div>
+                  <img src={caretdown} className='h-2 w-3' />
+                </div>
+              </div>
             </div>
           </div>
-          <div className='flex flex-col gap-2'>
-            <div className='font-poppins font-normal text-[13px] leading-relaxed text-[#505050]'>Company HQ Locations</div>
-            <div className='w-full flex items-center justify-between rounded-lg px-4 py-2 border-gray-200 border-2'>
-              <div className='font-poppins font-normal text-[13px] leading-relaxed text-[#6b6b6b]'>India...</div>
-              <img src={caretdown} className='h-2 w-3' />
-            </div>
-          </div>
-          <div className='flex flex-col gap-2'>
-            <div className='font-poppins font-normal text-[13px] leading-relaxed text-[#505050]'>Revenue range</div>
-            <div className='w-full flex items-center justify-between rounded-lg px-4 py-2 border-gray-200 border-2'>
-              <div className='font-poppins font-normal text-[13px] leading-relaxed text-[#6b6b6b]'>50L - 1Cr...</div>
-              <img src={caretdown} className='h-2 w-3' />
+          <div>
+            <div className='h-[345px] w-4 rounded-full' style={{ background: "var(--secondary, rgba(220, 220, 220, 1))" }} >
+              <div className='h-[100px] w-4 rounded-full' style={{ background: "var(--secondary,rgba(136, 138, 151, 1))" }}></div>
             </div>
           </div>
         </div>
 
-
+        <div className='font-poppins font-normal text-[18px] mt-[14px]'>Prospect Profile</div>
+        <div className='w-[95%] mt-[6px] border-gray-200 border-2 px-6 py-4 flex flex-col gap-6 rounded-xl'>
+          <div className='flex flex-col gap-2'>
+            <div className='font-poppins font-normal text-[13px]'>Department</div>
+            <div className='w-full flex items-center justify-between rounded-lg px-4 py-2 border-gray-200 border-2'>
+              <div className='font-poppins font-normal text-[13px] leading-relaxed text-[#8c8c8c]'>Search Department</div>
+              <img src={caretdown} className='h-2 w-3' />
+            </div>
+          </div>
+        </div>
       </div>
+
+
 
     </div>
   )
